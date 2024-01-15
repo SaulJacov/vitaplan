2eb06086036ed8f259f2676c1f76d22936e7eac2
CarlosAHT <hernandeztorresca12@gmail.com>
2024-01-15 00:38:32 -0600

    Correcciones de Texto

--- agendar_citas.php
+++ agendar_citas.php
@@ -15,7 +15,7 @@ $nutriologo = $_SESSION['usuario_nombre'];
 <html>
 <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>VitaPlan | Nutriólogo</title>
+        <title>VitaPlan | Agendar Citas</title>
         <link rel="icon" href="img/LogoP.jpg" type="image/jpg" />
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
