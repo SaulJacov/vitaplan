2d097baa154dae3a104da0f0f76b17f2614c02eb
SaulJacov <martinezjimenezsauljacob@gmail.com>
2024-01-14 18:34:55 -0600

    nueva base de datos

--- vitaplan.sql
+++ vitaplan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-01-2024 a las 07:35:55
+-- Tiempo de generación: 15-01-2024 a las 01:34:13
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -24,189 +24,80 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `anuel`
+-- Estructura de tabla para la tabla `gago`
 --
 
-CREATE TABLE `anuel` (
+CREATE TABLE `gago` (
   `id` int(6) UNSIGNED NOT NULL,
   `email` varchar(255) NOT NULL,
   `apellidoP` varchar(255) NOT NULL,
   `apellidoM` varchar(255) NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
   `telefono` varchar(20) NOT NULL,
   `especialidad` varchar(255) NOT NULL,
   `numero_identificacion` varchar(20) NOT NULL,
-  `curp` varchar(20) DEFAULT NULL
+  `curp` varchar(30) DEFAULT NULL,
+  `ID_paciente` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `anuel`
+-- Volcado de datos para la tabla `gago`
 --
 
-INSERT INTO `anuel` (`id`, `email`, `apellidoP`, `apellidoM`, `telefono`, `especialidad`, `numero_identificacion`, `curp`) VALUES
-(1, 'anuel aa', 'anuel aa', 'anuel aa', 'anuel', 'anuel', 'anuel', NULL);
+INSERT INTO `gago` (`id`, `email`, `apellidoP`, `apellidoM`, `nombre`, `telefono`, `especialidad`, `numero_identificacion`, `curp`, `ID_paciente`) VALUES
+(1, 'gago@gmail.com', 'ruben ', 'gago', NULL, '5555555555', 'tecnico', '37', NULL, NULL);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `anuel_aa`
+-- Estructura de tabla para la tabla `hierro`
 --
 
-CREATE TABLE `anuel_aa` (
+CREATE TABLE `hierro` (
   `id` int(6) UNSIGNED NOT NULL,
   `email` varchar(255) NOT NULL,
   `apellidoP` varchar(255) NOT NULL,
   `apellidoM` varchar(255) NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
   `telefono` varchar(20) NOT NULL,
   `especialidad` varchar(255) NOT NULL,
   `numero_identificacion` varchar(20) NOT NULL,
-  `curp` varchar(20) DEFAULT NULL
+  `curp` varchar(30) DEFAULT NULL,
+  `ID_paciente` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `anuel_aa`
+-- Volcado de datos para la tabla `hierro`
 --
 
-INSERT INTO `anuel_aa` (`id`, `email`, `apellidoP`, `apellidoM`, `telefono`, `especialidad`, `numero_identificacion`, `curp`) VALUES
-(1, 'anuel aa', 'anuel aa', 'anuel aa', 'anuel', 'anuel', 'anuel', NULL);
+INSERT INTO `hierro` (`id`, `email`, `apellidoP`, `apellidoM`, `nombre`, `telefono`, `especialidad`, `numero_identificacion`, `curp`, `ID_paciente`) VALUES
+(1, 'hierro@gmail.com', 'fernando', 'hierro', NULL, '5555555555', 'director deportivo', '55', NULL, NULL);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `isabel`
+-- Estructura de tabla para la tabla `jose`
 --
 
-CREATE TABLE `isabel` (
+CREATE TABLE `jose` (
   `id` int(6) UNSIGNED NOT NULL,
   `email` varchar(255) NOT NULL,
   `apellidoP` varchar(255) NOT NULL,
   `apellidoM` varchar(255) NOT NULL,
-  `telefono` varchar(20) NOT NULL,
-  `especialidad` varchar(255) NOT NULL,
-  `numero_identificacion` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `isabel`
---
-
-INSERT INTO `isabel` (`id`, `email`, `apellidoP`, `apellidoM`, `telefono`, `especialidad`, `numero_identificacion`) VALUES
-(1, 'isabel', 'isabel', 'isabel', 'isabel', 'isabel', '');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `jacov_saul`
---
-
-CREATE TABLE `jacov_saul` (
-  `id` int(6) UNSIGNED NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `apellidoP` varchar(255) NOT NULL,
-  `apellidoM` varchar(255) NOT NULL,
-  `telefono` varchar(20) NOT NULL,
-  `especialidad` varchar(255) NOT NULL,
-  `numero_identificacion` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `jacov_saul`
---
-
-INSERT INTO `jacov_saul` (`id`, `email`, `apellidoP`, `apellidoM`, `telefono`, `especialidad`, `numero_identificacion`) VALUES
-(1, 'miguelmiguel', 'miguel', 'miguelmiguel', 'miguel', 'miguel', 'miguel');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `jjjjjjjj`
---
-
-CREATE TABLE `jjjjjjjj` (
-  `id` int(6) UNSIGNED NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `apellidoP` varchar(255) NOT NULL,
-  `apellidoM` varchar(255) NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
   `telefono` varchar(20) NOT NULL,
   `especialidad` varchar(255) NOT NULL,
   `numero_identificacion` varchar(20) NOT NULL,
-  `curp` varchar(50) NOT NULL
+  `curp` varchar(30) DEFAULT NULL,
+  `ID_paciente` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `jjjjjjjj`
+-- Volcado de datos para la tabla `jose`
 --
 
-INSERT INTO `jjjjjjjj` (`id`, `email`, `apellidoP`, `apellidoM`, `telefono`, `especialidad`, `numero_identificacion`, `curp`) VALUES
-(1, 'jjjjjjjj', 'jjjjjjjj', 'jjjjjjjj', 'jjjjjjjj', 'jjjjjjjj', 'jjjjjjjj', '');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `joaquin`
---
-
-CREATE TABLE `joaquin` (
-  `id` int(6) UNSIGNED NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `apellidoP` varchar(255) NOT NULL,
-  `apellidoM` varchar(255) NOT NULL,
-  `telefono` varchar(20) NOT NULL,
-  `especialidad` varchar(255) NOT NULL,
-  `numero_identificacion` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `joaquin`
---
-
-INSERT INTO `joaquin` (`id`, `email`, `apellidoP`, `apellidoM`, `telefono`, `especialidad`, `numero_identificacion`) VALUES
-(1, 'joaquin', 'joaquin', 'joaquin', 'joaquin', 'joaquin', '');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `joel`
---
-
-CREATE TABLE `joel` (
-  `id` int(6) UNSIGNED NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `apellidoP` varchar(255) NOT NULL,
-  `apellidoM` varchar(255) NOT NULL,
-  `telefono` varchar(20) NOT NULL,
-  `especialidad` varchar(255) NOT NULL,
-  `numero_identificacion` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `joel`
---
-
-INSERT INTO `joel` (`id`, `email`, `apellidoP`, `apellidoM`, `telefono`, `especialidad`, `numero_identificacion`) VALUES
-(1, 'joel', 'joel', 'joel', 'joel', 'joel|', 'joel');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `juanito`
---
-
-CREATE TABLE `juanito` (
-  `id` int(6) UNSIGNED NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `apellidoP` varchar(255) NOT NULL,
-  `apellidoM` varchar(255) NOT NULL,
-  `telefono` varchar(20) NOT NULL,
-  `especialidad` varchar(255) NOT NULL,
-  `numero_identificacion` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `juanito`
---
-
-INSERT INTO `juanito` (`id`, `email`, `apellidoP`, `apellidoM`, `telefono`, `especialidad`, `numero_identificacion`) VALUES
-(1, 'juanito', 'juanito', 'juanito', 'juanito', 'juanito', '');
+INSERT INTO `jose` (`id`, `email`, `apellidoP`, `apellidoM`, `nombre`, `telefono`, `especialidad`, `numero_identificacion`, `curp`, `ID_paciente`) VALUES
+(1, 'admin@gmail.com', 'lopez', 'gonzalez', NULL, '1234567890|', 'doctor', '123', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -255,19 +146,13 @@ CREATE TABLE `nutriologos` (
 --
 
 INSERT INTO `nutriologos` (`id`, `email`, `nombre`, `apellidoP`, `apellidoM`, `telefono`, `especialidad`, `numeroIdentificacion`, `usuario`, `contraseña`) VALUES
-(1, '0', '0', '0', '0', '0', '0', '0', '0', '0'),
-(2, 'isabel', 'isabel', 'isabel', 'isabel', '0', '0', '0', '0', '0'),
-(3, 'maria', 'maria', 'maria', 'maria', 'v', 'maria', 'maria', 'maria', 'maria'),
 (4, 'lujan', 'lujan', 'lujan', 'lujan', 'lujan', 'lujan', 'lujan', 'lujan', 'lujan'),
-(5, 'joaquin', 'joaquin', 'joaquin', 'joaquin', 'joaquin', 'joaquin', '', 'joaquin', 'joaquin'),
-(6, 'jjjjjjjj', 'jjjjjjjj', 'jjjjjjjj', 'jjjjjjjj', 'jjjjjjjj', 'jjjjjjjj', 'jjjjjjjj', 'jjjjjjjj', 'jjjjjjjj'),
-(7, 'xxxxx', 'xxxxx', 'xxxxx', 'xxxxx', 'xxxxx', 'xxxxx', 'xxxxx', 'xxxxx', 'xxxxx'),
-(8, 'anuel aa', 'anuel aa', 'anuel aa', 'anuel aa', 'anuel', 'anuel', 'anuel', 'anuel', 'anuel'),
-(9, 'anuel aa', 'anuel', 'anuel aa', 'anuel aa', 'anuel', 'anuel', 'anuel', 'anuel', 'anuel'),
 (10, 'qwerty', 'qwerty', 'qwerty', 'qwerty', 'qwerty', 'qwerty', 'qwerty', 'qwerty', 'qwerty'),
 (11, 'jacksaul.86@gmail.com', 'ozuna', 'martinez', 'ocasio', '018007777', 'pediatra', '777', 'ozuna', 'kkkk'),
 (12, 'rauw@gmail.com', 'rauw', 'alejendro', 'bellako', '1234567890', 'infantil', '22', 'rauw', 'FEPI'),
-(13, 'www', 'www', 'www', 'www', 'www', 'www', 'www', 'www', 'www');
+(14, 'admin@gmail.com', 'jose', 'lopez', 'gonzalez', '1234567890|', 'doctor', '123', 'jose', '1234'),
+(16, 'gago@gmail.com', 'Gago', 'ruben ', 'gago', '5555555555', 'tecnico', '37', 'Gago', '1234'),
+(17, 'hierro@gmail.com', 'hierro', 'fernando', 'hierro', '5555555555', 'director deportivo', '55', 'Hierro', '1234');
 
 -- --------------------------------------------------------
 
@@ -324,30 +209,13 @@ CREATE TABLE `pacientes` (
 --
 
 INSERT INTO `pacientes` (`id_p`, `email`, `nombre`, `apellidoP`, `apellidoM`, `telefono`, `curp`, `contraseña`) VALUES
-(1, '0', '0', '0', '0', '0', '0', '0'),
-(2, '0', '0', '0', '0', '0', '0', '0'),
-(3, '', '', '', '', '', '', ''),
-(4, '', '', '', '', '', '', ''),
-(5, '', '', '', '', '', '', ''),
-(6, 'gato', 'gato', 'gato', 'gato', 'gato', 'gato', 'gato'),
-(7, 'paciente', 'paciente', 'paciente', 'paciente', 'paciente', 'paciente', 'paciente'),
-(8, '', '', '', '', '', '', ''),
-(9, '', '', '', '', '', '', ''),
-(10, '', '', '', '', '', '', ''),
-(11, 'jacov', 'jacov', 'jacov', 'jacov', 'jacov', 'jacov', 'jacov'),
-(12, 'jacov', 'jacov', 'jacov', 'jacov', 'jacov', 'jacov', 'rrrrrr'),
-(13, 'saul', 'saul', 'saul', 'saul', 'saul', 'saul', 'saul'),
-(14, '', '', '', '', '', '', ''),
-(15, '', '', '', '', '', '', ''),
-(16, '', '', '', '', '', '', ''),
 (17, 'asdfg', 'asdfg', 'asdfg', 'asdfg', 'asdfg', 'asdfg', 'asdfgasdfg'),
-(18, 'felipe', 'felipe', 'martinez ', 'osornio', '55555555555', 'FMO1972', 'qwerty'),
-(19, 'martinezjimenezsauljacob@gmail.com', 'wisin', 'yandel', 'wisin', '1234567890', 'wisin', '123456'),
-(20, 'martinezjimenezsauljacob@gmail.com', 'wisin', 'yandel', 'wisin', '1234567890', 'wisin', 'qwerty'),
-(21, 'asdfg@gmail.com', 'duki', 'ramirez', 'juarez', '5526644722', 'DRJ99', 'fepi'),
-(22, '1234', '1234', '1234', '1234', '1234', '1234', '1234'),
-(23, 'qqqq', 'qqqq', 'qqqq', 'qqqq', 'qqqq', 'qqqq', 'qqqq'),
-(24, 'brian@gmail.com', 'brian', 'brian', 'brian', 'brian', 'brian', 'brian');
+(24, 'brian@gmail.com', 'brian', 'brian', 'brian', 'brian', 'brian', 'brian'),
+(25, 'duki@gmail.com', 'duki', 'martinez', 'jimenez', '0987654321', 'DMJ', '1234'),
+(26, 'alexis@gmail.com', 'alexis', 'vega', 'rojas', '5555555555', 'AVR', '1234'),
+(27, 'yael@gmail.com', 'yael', 'padilla', 'sandoval', '5555555555', 'YPS', '1234'),
+(28, 'chicharito@gmail.com', 'javier', 'hernandez', 'balcazar', '5555555555', 'CH14', '1234'),
+(29, 'piojo@gmail.com', 'roberto', 'alvarado', 'hernandez', '5555555555', 'RAH', '1234');
 
 -- --------------------------------------------------------
 
@@ -401,107 +269,26 @@ INSERT INTO `rauw` (`id`, `email`, `apellidoP`, `apellidoM`, `nombre`, `telefono
 (2, 'asdfg@gmail.com', 'ramirez', 'juarez', 'duki', '5526644722', '', '', 'DRJ99', '21'),
 (3, 'asdfg', 'asdfg', 'asdfg', 'asdfg', 'asdfg', '', '', 'asdfg', '17');
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `www`
---
-
-CREATE TABLE `www` (
-  `id` int(6) UNSIGNED NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `apellidoP` varchar(255) NOT NULL,
-  `apellidoM` varchar(255) NOT NULL,
-  `nombre` varchar(255) DEFAULT NULL,
-  `telefono` varchar(20) NOT NULL,
-  `especialidad` varchar(255) NOT NULL,
-  `numero_identificacion` varchar(20) NOT NULL,
-  `curp` varchar(30) DEFAULT NULL,
-  `ID_paciente` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `www`
---
-
-INSERT INTO `www` (`id`, `email`, `apellidoP`, `apellidoM`, `nombre`, `telefono`, `especialidad`, `numero_identificacion`, `curp`, `ID_paciente`) VALUES
-(1, 'www', 'www', 'www', NULL, 'www', 'www', 'www', NULL, NULL),
-(2, 'qqqq', 'qqqq', 'qqqq', 'qqqq', 'qqqq', '', '', 'qqqq', '23');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `xxxxx`
---
-
-CREATE TABLE `xxxxx` (
-  `id` int(6) UNSIGNED NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `apellidoP` varchar(255) NOT NULL,
-  `apellidoM` varchar(255) NOT NULL,
-  `telefono` varchar(20) NOT NULL,
-  `especialidad` varchar(255) NOT NULL,
-  `numero_identificacion` varchar(20) NOT NULL,
-  `curp` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `xxxxx`
---
-
-INSERT INTO `xxxxx` (`id`, `email`, `apellidoP`, `apellidoM`, `telefono`, `especialidad`, `numero_identificacion`, `curp`) VALUES
-(1, 'xxxxx', 'xxxxx', 'xxxxx', 'xxxxx', 'xxxxx', 'xxxxx', NULL);
-
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `anuel`
+-- Indices de la tabla `gago`
 --
-ALTER TABLE `anuel`
+ALTER TABLE `gago`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `anuel_aa`
+-- Indices de la tabla `hierro`
 --
-ALTER TABLE `anuel_aa`
+ALTER TABLE `hierro`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `isabel`
+-- Indices de la tabla `jose`
 --
-ALTER TABLE `isabel`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `jacov_saul`
---
-ALTER TABLE `jacov_saul`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `jjjjjjjj`
---
-ALTER TABLE `jjjjjjjj`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `joaquin`
---
-ALTER TABLE `joaquin`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `joel`
---
-ALTER TABLE `joel`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `juanito`
---
-ALTER TABLE `juanito`
+ALTER TABLE `jose`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -541,67 +328,25 @@ ALTER TABLE `rauw`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `www`
---
-ALTER TABLE `www`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `xxxxx`
---
-ALTER TABLE `xxxxx`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `anuel`
+-- AUTO_INCREMENT de la tabla `gago`
 --
-ALTER TABLE `anuel`
+ALTER TABLE `gago`
   MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT de la tabla `anuel_aa`
+-- AUTO_INCREMENT de la tabla `hierro`
 --
-ALTER TABLE `anuel_aa`
+ALTER TABLE `hierro`
   MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT de la tabla `isabel`
+-- AUTO_INCREMENT de la tabla `jose`
 --
-ALTER TABLE `isabel`
-  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT de la tabla `jacov_saul`
---
-ALTER TABLE `jacov_saul`
-  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT de la tabla `jjjjjjjj`
---
-ALTER TABLE `jjjjjjjj`
-  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT de la tabla `joaquin`
---
-ALTER TABLE `joaquin`
-  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT de la tabla `joel`
---
-ALTER TABLE `joel`
-  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT de la tabla `juanito`
---
-ALTER TABLE `juanito`
+ALTER TABLE `jose`
   MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
@@ -614,7 +359,7 @@ ALTER TABLE `lujan`
 -- AUTO_INCREMENT de la tabla `nutriologos`
 --
 ALTER TABLE `nutriologos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT de la tabla `ozuna`
@@ -626,7 +371,7 @@ ALTER TABLE `ozuna`
 -- AUTO_INCREMENT de la tabla `pacientes`
 --
 ALTER TABLE `pacientes`
-  MODIFY `id_p` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id_p` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT de la tabla `qwerty`
@@ -639,18 +384,6 @@ ALTER TABLE `qwerty`
 --
 ALTER TABLE `rauw`
   MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT de la tabla `www`
---
-ALTER TABLE `www`
-  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT de la tabla `xxxxx`
---
-ALTER TABLE `xxxxx`
-  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
