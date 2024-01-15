0cf6a6c098d340dcf227ba4982f01e55172797a4
SaulJacov <martinezjimenezsauljacob@gmail.com>
2024-01-15 02:22:13 -0600

    database actualizada
    esta es la ultima base de datos

--- vitaplan.sql
+++ vitaplan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-01-2024 a las 01:34:13
+-- Tiempo de generación: 15-01-2024 a las 09:20:55
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -20,6 +20,33 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `vitaplan`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `citas`
+--
+
+CREATE TABLE `citas` (
+  `id_cita` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `hora` time NOT NULL,
+  `nutriologo` varchar(255) NOT NULL,
+  `id_p` int(11) NOT NULL,
+  `curp` varchar(255) NOT NULL,
+  `nombre_p` varchar(255) NOT NULL,
+  `email_p` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `citas`
+--
+
+INSERT INTO `citas` (`id_cita`, `fecha`, `hora`, `nutriologo`, `id_p`, `curp`, `nombre_p`, `email_p`) VALUES
+(6, '2024-01-15', '17:00:00', 'rauw', 26, 'AVR', 'alexis.vega.rojas', 'alexis@gmail.com'),
+(7, '2024-01-20', '17:00:00', 'rauw', 28, 'CH14', 'javier.hernandez.balcazar', 'chicharito@gmail.com'),
+(8, '2024-01-30', '09:00:00', 'rauw', 28, 'CH14', 'javier.hernandez.balcazar', 'chicharito@gmail.com'),
+(9, '2024-01-16', '10:00:00', 'wisin ', 32, 'LAPRESION', 'jhay.cortes.presion', 'jhayco@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -152,7 +179,9 @@ INSERT INTO `nutriologos` (`id`, `email`, `nombre`, `apellidoP`, `apellidoM`, `t
 (12, 'rauw@gmail.com', 'rauw', 'alejendro', 'bellako', '1234567890', 'infantil', '22', 'rauw', 'FEPI'),
 (14, 'admin@gmail.com', 'jose', 'lopez', 'gonzalez', '1234567890|', 'doctor', '123', 'jose', '1234'),
 (16, 'gago@gmail.com', 'Gago', 'ruben ', 'gago', '5555555555', 'tecnico', '37', 'Gago', '1234'),
-(17, 'hierro@gmail.com', 'hierro', 'fernando', 'hierro', '5555555555', 'director deportivo', '55', 'Hierro', '1234');
+(17, 'hierro@gmail.com', 'hierro', 'fernando', 'hierro', '5555555555', 'director deportivo', '55', 'Hierro', '1234'),
+(18, 'wisin@gmail.com', 'wisin ', 'yandel', 'elQuimico', '222222222222', 'perreo', '111111', 'wisin', '1234'),
+(19, 'wisin@gmail.com', 'wisin', 'yandel', 'elQuimico', '222222222222', 'perreo', '111111', 'wisin', '1234');
 
 -- --------------------------------------------------------
 
@@ -215,7 +244,10 @@ INSERT INTO `pacientes` (`id_p`, `email`, `nombre`, `apellidoP`, `apellidoM`, `t
 (26, 'alexis@gmail.com', 'alexis', 'vega', 'rojas', '5555555555', 'AVR', '1234'),
 (27, 'yael@gmail.com', 'yael', 'padilla', 'sandoval', '5555555555', 'YPS', '1234'),
 (28, 'chicharito@gmail.com', 'javier', 'hernandez', 'balcazar', '5555555555', 'CH14', '1234'),
-(29, 'piojo@gmail.com', 'roberto', 'alvarado', 'hernandez', '5555555555', 'RAH', '1234');
+(29, 'piojo@gmail.com', 'roberto', 'alvarado', 'hernandez', '5555555555', 'RAH', '1234'),
+(30, 'jhayco', 'jhay', 'cortez', 'lapresion', '5555555555', 'JCLP', '1234'),
+(31, 'jhayco', 'jhay', 'cortez', 'lapresion', '5555555555', 'JCLP', '1234'),
+(32, 'jhayco@gmail.com', 'jhay', 'cortes', 'presion', '000000000000', 'LAPRESION', '1234');
 
 -- --------------------------------------------------------
 
@@ -267,11 +299,75 @@ CREATE TABLE `rauw` (
 INSERT INTO `rauw` (`id`, `email`, `apellidoP`, `apellidoM`, `nombre`, `telefono`, `especialidad`, `numero_identificacion`, `curp`, `ID_paciente`) VALUES
 (1, 'rauw@gmail.com', 'alejendro', 'bellako', NULL, '1234567890', 'infantil', '22', NULL, NULL),
 (2, 'asdfg@gmail.com', 'ramirez', 'juarez', 'duki', '5526644722', '', '', 'DRJ99', '21'),
-(3, 'asdfg', 'asdfg', 'asdfg', 'asdfg', 'asdfg', '', '', 'asdfg', '17');
+(3, 'asdfg', 'asdfg', 'asdfg', 'asdfg', 'asdfg', '', '', 'asdfg', '17'),
+(4, 'duki@gmail.com', 'martinez', 'jimenez', 'duki', '0987654321', '', '', 'DMJ', '25'),
+(5, 'chicharito@gmail.com', 'hernandez', 'balcazar', 'javier', '5555555555', '', '', 'CH14', '28'),
+(6, 'piojo@gmail.com', 'alvarado', 'hernandez', 'roberto', '5555555555', '', '', 'RAH', '29'),
+(7, 'brian@gmail.com', 'brian', 'brian', 'brian', 'brian', '', '', 'brian', '24');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `wisin`
+--
+
+CREATE TABLE `wisin` (
+  `id` int(6) UNSIGNED NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `apellidoP` varchar(255) NOT NULL,
+  `apellidoM` varchar(255) NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `telefono` varchar(20) NOT NULL,
+  `especialidad` varchar(255) NOT NULL,
+  `numero_identificacion` varchar(20) NOT NULL,
+  `curp` varchar(30) DEFAULT NULL,
+  `ID_paciente` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `wisin`
+--
+
+INSERT INTO `wisin` (`id`, `email`, `apellidoP`, `apellidoM`, `nombre`, `telefono`, `especialidad`, `numero_identificacion`, `curp`, `ID_paciente`) VALUES
+(1, 'wisin@gmail.com', 'yandel', 'elQuimico', NULL, '222222222222', 'perreo', '111111', NULL, NULL),
+(2, 'jhayco', 'cortez', 'lapresion', 'jhay', '5555555555', '', '', 'JCLP', '30'),
+(3, 'jhayco@gmail.com', 'cortes', 'presion', 'jhay', '000000000000', '', '', 'LAPRESION', '32');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `wisin_`
+--
+
+CREATE TABLE `wisin_` (
+  `id` int(6) UNSIGNED NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `apellidoP` varchar(255) NOT NULL,
+  `apellidoM` varchar(255) NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `telefono` varchar(20) NOT NULL,
+  `especialidad` varchar(255) NOT NULL,
+  `numero_identificacion` varchar(20) NOT NULL,
+  `curp` varchar(30) DEFAULT NULL,
+  `ID_paciente` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `wisin_`
+--
+
+INSERT INTO `wisin_` (`id`, `email`, `apellidoP`, `apellidoM`, `nombre`, `telefono`, `especialidad`, `numero_identificacion`, `curp`, `ID_paciente`) VALUES
+(1, 'wisin@gmail.com', 'yandel', 'elQuimico', NULL, '222222222222', 'perreo', '111111', NULL, NULL);
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `citas`
+--
+ALTER TABLE `citas`
+  ADD PRIMARY KEY (`id_cita`);
 
 --
 -- Indices de la tabla `gago`
@@ -328,8 +424,26 @@ ALTER TABLE `rauw`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `wisin`
+--
+ALTER TABLE `wisin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `wisin_`
+--
+ALTER TABLE `wisin_`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `citas`
+--
+ALTER TABLE `citas`
+  MODIFY `id_cita` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `gago`
@@ -359,7 +473,7 @@ ALTER TABLE `lujan`
 -- AUTO_INCREMENT de la tabla `nutriologos`
 --
 ALTER TABLE `nutriologos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT de la tabla `ozuna`
@@ -371,7 +485,7 @@ ALTER TABLE `ozuna`
 -- AUTO_INCREMENT de la tabla `pacientes`
 --
 ALTER TABLE `pacientes`
-  MODIFY `id_p` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id_p` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT de la tabla `qwerty`
@@ -383,7 +497,19 @@ ALTER TABLE `qwerty`
 -- AUTO_INCREMENT de la tabla `rauw`
 --
 ALTER TABLE `rauw`
+  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `wisin`
+--
+ALTER TABLE `wisin`
   MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `wisin_`
+--
+ALTER TABLE `wisin_`
+  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
